b37ac52a790df2bebd2684870960e36a67475881
Sreekanth <sreekanth100k@gmail.com>
2020-12-03 15:32:11 +0530

    Enter title and notes screen done

---
+++ MyApplication3/app/src/main/java/com/example/myapplication/EnterTitleAndNotesActivity.kt
@@ -0,0 +1,33 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.doOnTextChanged
+
+class EnterTitleAndNotesActivity : AppCompatActivity() {
+
+   lateinit var mContentEt:EditText
+   lateinit var mTitleEt:EditText
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.enter_title_and_notes_activity)
+
+       mContentEt  = findViewById(R.id.id_content_et)
+
+        mTitleEt    =   findViewById(R.id.id_title_et)
+
+        mContentEt.doOnTextChanged { text, start, before, count ->  }
+
+
+        mTitleEt.doOnTextChanged { text, start, before, count ->  }
+
+
+
+
+
+    }
+}
